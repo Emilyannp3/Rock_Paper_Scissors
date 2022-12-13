db1650526805c0e4aa5a6471f7de7482828d138f
Emily <emily1plummer@gmail.com>
2022-12-13 12:14:31 -0600

    added random generator for script

--- rockjavascript.js
+++ rockjavascript.js
@@ -1,2 +1,20 @@
 //creating rock app
-function getComputerChoice()[
+//make a user string input as prompt
+//have computer randomly choose string
+//If string === string then return Draw
+//If User Rock and Computer Scissors return Win else lose
+
+//If User Paper and Computer Rock return Win else lose
+
+parseFloat(prompt("Enter your choice"))
+
+
+function getComputerChoice(arr){
+const randomIndexChoice = Math.floor(Math.random() * arr.length);
+const choice = arr[randomIndexChoice];
+return choice;
+
+}
+const arr = ["Rock", "Paper", "Scissors"];
+const result = getComputerChoice(arr);
+console.log(result);
