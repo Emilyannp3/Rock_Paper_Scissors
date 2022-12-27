2547fb06abd2a93e980f925912a433ded918e0e4
Emily <emily1plummer@gmail.com>
2022-12-27 13:56:08 -0600

    added scoring to the app

--- rockjavascript.js
+++ rockjavascript.js
@@ -22,35 +22,54 @@ function getComputerChoice() {
     
 }
 let computerSelection = getComputerChoice();
+let winScore = 1;
+   let loseScore = 1;
+   let drawScore = 1;
 
 function playRound(){
  
 
     if (playerSelection == "Rock" && computerSelection == "Paper") {
-        return "You Lose, Computer Wins";
+        return "You Lose" + loseScore++;
     } else if (playerSelection == "Scissors" && computerSelection == "Paper"){
-        return "You win";
+        return "You Win" + winScore++;
     } else if (playerSelection == "Paper" && computerSelection == "Rock"){
-        return "You Win";
+        return "You Win" + winScore++;
     }else if (playerSelection == "Rock" && computerSelection == "Scissors"){
-        return "You win";
+        return "You Win" + winScore++;
     } else if (playerSelection == "Scissors" && computerSelection == "Rock"){
-        return "You Lose";
+        return "You Lose" + loseScore++;
     } else if (playerSelection == "Paper" && computerSelection == "Scissors"){
-        return "You Win";
+        return "You Lose" + loseScore++;
     }else {
-        return "Draw";
+        return "Draw" + drawScore++;
     }
-
+  
 }
+//let roundScore = playRound();
+//function takeScore(){
+ //   let winScore = 0;
+ //   let loseScore = 0;
+ //   let drawScore = 0;
+
+ //   if (roundScore = "You Lose") {
+ //       return loseScore ++ ;
+   // } else if (roundScore = "You Win") {
+  //      return winScore ++ ;
+  //  } else{
+  //      return drawScore ++;
+  //  }
+
+//}
 
 function game(){
-for (let i = 0; i < 5; i++){
+for (let i = 1; i < 5; i++){
     prompt("Enter your choice");
     console.log(playerSelection);
     computerSelection = getComputerChoice();
     console.log(computerSelection);
     console.log(playRound());
+    
 }
 }
 game();
