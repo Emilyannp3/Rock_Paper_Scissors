2d2fce1a421a6dc10c21e4d699e0f2a718527863
Emily <emily1plummer@gmail.com>
2022-12-28 08:13:21 -0600

    erased all unneeded comments

--- rockjavascript.js
+++ rockjavascript.js
@@ -1,12 +1,6 @@
 //creating rock app
-//make a user string input as prompt
-//have computer randomly choose string
-//If string === string then return Draw
-//If User Rock and Computer Scissors return Win else lose
 
-//If User Paper and Computer Rock return Win else lose
-
-let playerSelection = prompt("Enter your choice");//could have just capitalized or lowerCased all letters here
+let playerSelection = prompt("Enter your choice");
 
 function capitalizeSelection(){
 playerSelection = playerSelection[0].toUpperCase() + playerSelection.substring(1).toLowerCase();
@@ -46,21 +40,6 @@ function playRound(){
     }
   
 }
-//let roundScore = playRound();
-//function takeScore(){
- //   let winScore = 0;
- //   let loseScore = 0;
- //   let drawScore = 0;
-
- //   if (roundScore = "You Lose") {
- //       return loseScore ++ ;
-   // } else if (roundScore = "You Win") {
-  //      return winScore ++ ;
-  //  } else{
-  //      return drawScore ++;
-  //  }
-
-//}
 
 function game(){
 for (let i = 1; i < 5; i++){
